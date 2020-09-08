2216947bfca2d8dc6a6d10815096c6ce9b49accc
Arjun Biddanda <aabiddanda@gmail.com>
2020-09-08 09:50:18 -0500

    added new package for public facing aDNA and LD analysis

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '0.0.1'
+
+required = open('requirements.txt').read().split('\n')
+
+setup(
+    name='aDNA_LD_public',
+    version=version,
+    description=' ',
+    author='Arjun Biddanda',
+    author_email='aabiddanda@gmail.com',
+    url='https://github.com/Arjun Biddanda/aDNA_LD_public',
+    packages=['aDNA_LD_public'],
+    install_requires=required,
+    long_description='See ' + 'https://github.com/Arjun Biddanda/aDNA_LD_public',
+    license='MIT'
+)
