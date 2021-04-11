95f441d243e1c01484eab7f43fa5173fdbd0f327
Arjun Biddanda <abiddanda@uchicago.edu>
2021-04-11 00:26:50 -0500

    got some more raw simulations that are closer to the actual estimation procedure

--- snakefiles/linkage_stats_sims.smk
+++ snakefiles/linkage_stats_sims.smk
@@ -28,6 +28,29 @@ seeds = [i+1 for i in range(nreps)]
 
 ###### --------- Estimating LD-statistics from haplotype copying simulations ----------- ######
 
+rule est_LDjt_stats_raw:
+  input:
+    hap_panel = rules.sim_demography_single_ancients.output.hap_panel
+  output:
+    ld_stats = config['tmpdir'] + 'ld_stats_raw/{scenario}/jointLDstats_mod{mod_n,\d+}_anc{n_anc}_t{ta,\d+}_l{length,\d+}_Ne{Ne,\d+}_{seed,\d+}_maf{maf,\d+}_polytotal{polytot,\d+}.npz'
+  run:
+    haps_df = np.load(input.hap_panel)
+    maf = int(wildcards.maf) / 100.
+    haps = haps_df['haps']
+    polytot = bool(wildcards.polytot)
+    times = haps_df['ta']
+    rec_pos = haps_df['rec_pos']
+    phys_pos = haps_df['phys_pos']
+    scenario = haps_df['scenario']
+    seed = haps_df['seed']
+    ta = int(wildcards.ta)
+    # Actually compute the joint product of LD
+    pABmod, pABanc,pAmod, pAanc, pBmod, pBanc, Dmod, Danc, gen_dist = TimeStratifiedLDStats.time_strat_hap_freq(haps, rec_pos, times, ta=ta, maf=maf, m=10000, polymorphic_total=polytot, seed=seed)
+    # Save the function here ...
+    np.savez_compressed(output.ld_stats, pABmod=pABmod, pABanc=pABanc, pAmod=pAmod, pBmod=pBmod, pAanc=pAanc, pBanc=pBanc, Dmod=Dmod, Danc=Danc, rec_dist=gen_dist, ta=ta, scenario=scenario, seed=seed)
+
+
+
 rule est_LDjt_stats:
   input:
     hap_panel = rules.sim_demography_single_ancients.output.hap_panel
@@ -51,4 +74,7 @@ rule est_LDjt_stats:
 
 rule run_est_jtLDstats:
   input:
-    expand(config['tmpdir'] + 'ld_stats/{scenario}/jointLDstats_mod{mod_n}_anc{n_anc}_t{ta}_l{length}_Ne{Ne}_{seed}_maf{maf}_polytotal{polytot}.npz', scenario='SerialConstant', ta=100, mod_n=100, n_anc=100, seed=seeds[0], length=5, Ne=10000, maf=[1,5], polytot=[0,1])
+#     expand(config['tmpdir'] + 'ld_stats/{scenario}/jointLDstats_mod{mod_n}_anc{n_anc}_t{ta}_l{length}_Ne{Ne}_{seed}_maf{maf}_polytotal{polytot}.npz', scenario='SerialConstant', ta=100, mod_n=500, n_anc=500, seed=seeds[0], length=1, Ne=10000, maf=[1,5], polytot=[0,1]),
+#     expand(config['tmpdir'] + 'ld_stats_raw/{scenario}/jointLDstats_mod{mod_n}_anc{n_anc}_t{ta}_l{length}_Ne{Ne}_{seed}_maf{maf}_polytotal{polytot}.npz', scenario='SerialConstant', ta=100, mod_n=100, n_anc=100, seed=seeds[0], length=[1], Ne=10000, maf=[1,5,10], polytot=[0,1]),
+#     expand(config['tmpdir'] + 'ld_stats_raw/{scenario}/jointLDstats_mod{mod_n}_anc{n_anc}_t{ta}_l{length}_Ne{Ne}_{seed}_maf{maf}_polytotal{polytot}.npz', scenario='SerialConstant', ta=100, mod_n=500, n_anc=500, seed=seeds, length=[1], Ne=10000, maf=[5], polytot=[0,1]),
+    expand(config['tmpdir'] + 'ld_stats_raw/{scenario}/jointLDstats_mod{mod_n}_anc{n_anc}_t{ta}_l{length}_Ne{Ne}_{seed}_maf{maf}_polytotal{polytot}.npz', scenario='SerialConstant', ta=[0,100,1000], mod_n=500, n_anc=500, seed=seeds[:10], length=[1], Ne=10000, maf=[5], polytot=[0,1])
--- src/time_strat_ld.py
+++ src/time_strat_ld.py
@@ -16,8 +16,13 @@ class TimeStratifiedLDStats:
         assert ta >= 0
         assert haps.shape[0] % 2 == 0
         assert (maf >= 0) and (maf < 0.5)
-        mod_idx = np.where(times == 0)[0]
-        anc_idx = np.where(times == ta)[0]
+        if ta == 0:
+          x = int(haps.shape[0]/2)
+          mod_idx = np.arange(x)
+          anc_idx = np.arange(x, haps.shape[0])
+        else:
+          mod_idx = np.where(times == 0)[0]
+          anc_idx = np.where(times == ta)[0]
         gt_mod = haps[mod_idx, :]
         af_mod = np.mean(gt_mod, axis=0)
         gt_anc = haps[anc_idx, :]
@@ -52,7 +57,118 @@ class TimeStratifiedLDStats:
         # We should grab the maximal distance here ... 
 #         idx = np.where(dist < max_dist)
         return (eD0Dt_norm[(dist < max_dist)], dist[(dist < max_dist)], af_mod, af_anc)
-
-    def joint_r2(haps, gen_pos, times):
-        """Calculate r2 in the joint time-stratified setting."""
-        pass
+    
+    def time_strat_hap_freq(haps, gen_pos, times, ta=100, maf=0.001, dist_bins = None, m = 100, polymorphic_total=True, seed=42):
+        """Calculate the haplotype frequencies within specific distance bins.
+        
+        Arguments:
+        ----------
+        
+        * haps: (np.array) - a N x M array of haplotypes
+        * gen_pos: (np.array) - length M vectors of genetic positions
+        * times: (np.array) - length N vector of times for each haploid sequencing
+        * ta: (int) - timepoint for ancient simulations
+        * maf: (float) - minimum minor allele frequency here
+        * dist_bins (np.array) - distance bins for estimating the haplotype frequencies 
+        * m (int) - number of monte-carlo samples per
+        
+        Returns:
+        --------
+        
+        * xxx
+        * xxx
+        
+        """
+        assert haps.shape[0] == times.size
+        assert haps.shape[1] == gen_pos.size
+        assert ta >= 0
+        assert haps.shape[0] % 2 == 0
+        assert (maf >= 0) and (maf < 0.5)
+        np.random.seed(seed)
+        if ta == 0:
+          x = int(haps.shape[0]/2)
+          mod_idx = np.arange(x)
+          anc_idx = np.arange(x, haps.shape[0])
+        else:
+          mod_idx = np.where(times == 0)[0]
+          anc_idx = np.where(times == ta)[0]
+        gt_mod = haps[mod_idx, :]
+        af_mod = np.mean(gt_mod, axis=0)
+        gt_anc = haps[anc_idx, :]
+        af_anc = np.mean(gt_anc, axis=0)
+        gt_tot = np.vstack([gt_mod, gt_anc])
+        af_tot = np.mean(gt_tot, axis=0)
+        if polymorphic_total:
+            af_filt_jt = (af_tot > maf) & (af_tot < 1.0 - maf)
+        else:
+            af_filt_jt = (
+                (af_mod > maf)
+                & (af_anc > maf)
+                & (af_mod < 1.0 - maf)
+                & (af_anc < 1.0 - maf)
+            )
+        # Perform some filtering using the MAF filters ...
+        pos_filt = gen_pos[af_filt_jt]
+        # We have to filter the genotype matrices as well...
+        gt_mod = gt_mod[:, af_filt_jt]
+        gt_anc = gt_anc[:, af_filt_jt]
+        dist = np.zeros((pos_filt.size, pos_filt.size), dtype=np.float32)
+        for i in range(pos_filt.size):
+          dist[i,:] = np.sqrt((pos_filt[i] - pos_filt)**2)
+        if dist_bins is None:
+          dist_bins = np.linspace(dist.min(), dist.max(), 50)
+        assert dist_bins.size > 2
+        pABmod = []
+        pABanc = []
+        pAmod = []
+        pBmod = []
+        pAanc = []
+        pBanc = []
+        Danc = []
+        Dmod = []
+        gen_dist = []
+        for i in tqdm(range(1, dist_bins.size)):
+          # Get indices in this range
+          idx1, idx2 = np.where((dist > dist_bins[i-1]) & (dist < dist_bins[i]))
+          # sample K haplotype pairs via Monte-Carlo
+          for j in np.random.choice(idx1.size, size=m):
+              # Choose the same two snps ... 
+              mod_haps = gt_mod[:, [idx1[j], idx2[j]]]
+              anc_haps = gt_anc[:, [idx1[j], idx2[j]]]
+              
+              # Calculate the relevant haplotype frequencies ... 
+              pAB_mod = np.sum((mod_haps[:,0] == 1) & (mod_haps[:,1] == 1))  / mod_idx.size
+              pAB_anc = np.sum((anc_haps[:,0] == 1) & (anc_haps[:,1] == 1))  / anc_idx.size
+              pA_mod = np.sum(mod_haps[:,0]) / mod_idx.size
+              pB_mod = np.sum(mod_haps[:,1]) / mod_idx.size
+              pA_anc = np.sum(anc_haps[:,0]) / anc_idx.size
+              pB_anc = np.sum(anc_haps[:,1]) / anc_idx.size
+              assert pAB_mod <= np.min([pA_mod, pB_mod])
+              assert pAB_anc <= np.min([pA_anc, pB_anc])
+              # Calculate the ancient and modern LD statistics ... 
+              D_mod = pAB_mod - pA_mod*pB_mod
+              D_anc= pAB_anc - pA_anc*pB_anc
+              pABmod.append(pAB_mod)
+              pABanc.append(pAB_anc)
+              pAmod.append(pA_mod)
+              pBmod.append(pB_mod)
+              pAanc.append(pA_anc)
+              pBanc.append(pB_anc)
+              Dmod.append(D_mod)
+              Danc.append(D_anc)
+              gen_dist.append(dist[idx1[j], idx2[j]])
+        # Convert all to numpy arrays
+        pABmod = np.array(pABmod)
+        pABanc = np.array(pABanc)
+        pAmod = np.array(pAmod)
+        pBmod = np.array(pBmod)
+        pAanc = np.array(pAanc)
+        pBanc = np.array(pBanc)
+        Dmod = np.array(Dmod)
+        Danc = np.array(Danc)
+        gen_dist = np.array(gen_dist)
+        # Make sure all the positions are positive numbers
+        assert np.all(gen_dist >= 0)
+        # Return all of the statistics as numpy arrays 
+        return(pABmod, pABanc,pAmod, pAanc, pBmod, pBanc, Dmod, Danc, gen_dist)
+          
